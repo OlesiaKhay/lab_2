fe6ff51839a15f536f839c6d10deff154dddb28a
OlesiaKhay <148855170+OlesiaKhay@users.noreply.github.com>
2023-11-05 12:44:10 +0300

    Update Продолжение финансового планирования.py

--- lab 2/Продолжение финансового планирования.py
+++ lab 2/Продолжение финансового планирования.py
@@ -5,18 +5,17 @@ increase = 0.03  # Ежемесячный рост цен
 
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 
-money_capital = 0
-d = 0
+difference = 0 #разница между тратами и зп
+money_capital = 0 #финансовая подушка безопасности
 i = 0
 
 while True:
-    money_capital = spend * (1 + increase) ** i - salary
+    difference = spend * (1 + increase) ** i - salary
 
 
     if i == months:
         break
     i += 1
-    d = d + money_capital
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов: {d:.2f}")
-
+    money_capital = money_capital + difference
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов: {money_capital:.2f}")
 
